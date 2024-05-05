62b6b991373967eb0e4ee883b103eba06922582b
Cliff <developerlyk@gmail.com>
2024-05-05 21:23:54 +0900

    Feat: Order,OrderItem entity 추가

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop.domain;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
@@ -17,6 +19,8 @@ import java.util.List;
 @Table(name = "orders")
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+
 public class Order {
   @Id
   @GeneratedValue
@@ -44,14 +48,50 @@ public class Order {
     this.member = member;
     member.getOrders().add(this);
   }
+
   //  연관관계 메서드 //
   public void addOrderItem(OrderItem orderItem) {
     orderItems.add(orderItem);
     orderItem.setOrder(this);
   }
+
   //  연관관계 메서드 //
   public void setDelivery(Delivery delivery) {
     this.delivery = delivery;
     delivery.setOrder(this);
   }
+
+  // 생성 메서드
+  public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+    Order order = new Order();
+    order.setMember(member);
+    order.setDelivery(delivery);
+    for (OrderItem orderItem : orderItems) {
+      order.addOrderItem(orderItem);
+    }
+    order.setStatus(OrderStatus.ORDER);
+    order.setOrderDate(LocalDateTime.now());
+    return order;
+  }
+  
+  // 주문 취소
+  public void cancel(){
+    if (delivery.getStatus() == DeliveryStatus.COMP){
+      throw new IllegalStateException("이미 배송 완료 된 상품은 취소 불가합니다.");
+    }
+    this.setStatus(OrderStatus.CANCEL);
+    for (OrderItem orderItem : orderItems) {
+      orderItem.cancel();
+    }
+  }
+  
+  // 조회 로직
+  public int getTotalPrice(){
+    int totalPrice = 0;
+    for (OrderItem orderItem : orderItems) {
+      totalPrice += orderItem.getTotalPrice();
+    }
+    return totalPrice;
+  }
+
 }
