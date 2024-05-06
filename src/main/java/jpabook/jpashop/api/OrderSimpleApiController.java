ce1f823242ba6e076aa6c8544b1e17aa8d420a7d
Cliff <developerlyk@gmail.com>
2024-05-06 18:45:43 +0900

    Feat: fetch join v4 dto로 가져오기

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,25 +1,23 @@
 package jpabook.jpashop.api;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.OrderSimpleQueryDto;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.stream.Collectors;
-
 /**
- * jpabook.jpashop.api OrderSimpleApiController
- * xtoOne에서 성능 최적화
- * order
- * order->member
- * order->delivery
+ * jpabook.jpashop.api OrderSimpleApiController xtoOne에서 성능 최적화 order order->member order->delivery
+ *
  * @author : K
  */
 @RestController
@@ -28,47 +26,52 @@ public class OrderSimpleApiController {
 
   private final OrderRepository orderRepository;
 
-  //무한 루프에 빠진다 order에서 member를 찾고 member에서 order를 찾고
+  // 무한 루프에 빠진다 order에서 member를 찾고 member에서 order를 찾고
   @GetMapping("/api/v1/simple-orders")
-  public List<Order> ordersv1(){
+  public List<Order> ordersv1() {
     List<Order> all = orderRepository.findAllByString(new OrderSearch());
     for (Order order : all) {
-      order.getMember().getName(); //Lazy 강제 초기화
+      order.getMember().getName(); // Lazy 강제 초기화
       order.getDelivery().getAddress(); // Lazy 강제 초기화
     }
     return all;
   }
-  
+
   @GetMapping("/api/v2/simple-orders")
-  public List<SimpleOrderDto> ordersV2(){
-    
+  public List<SimpleOrderDto> ordersV2() {
+
     return orderRepository.findAllByString(new OrderSearch()).stream()
-            .map(SimpleOrderDto::new)
-            .collect(Collectors.toList());
-  }
-  
-  @GetMapping("/api/v3/simple-orders")
-  public List<SimpleOrderDto> ordersV3(){
-    
-    return orderRepository.findAllWithMemberDelivery().stream().map(o -> new SimpleOrderDto(o)).collect(Collectors.toList());
+        .map(SimpleOrderDto::new)
+        .collect(Collectors.toList());
   }
 
-@Data
-  static class SimpleOrderDto{
+  @GetMapping("/api/v3/simple-orders")
+  public List<SimpleOrderDto> ordersV3() {
+
+    return orderRepository.findAllWithMemberDelivery().stream()
+        .map(o -> new SimpleOrderDto(o))
+        .collect(Collectors.toList());
+  }
+
+  @GetMapping("/api/v4/simple-orders")
+  public List<OrderSimpleQueryDto> ordersV4() {
+    return orderRepository.findOrderDtos();
+  }
+
+  @Data
+  static class SimpleOrderDto {
     private Long orderId;
     private String name;
     private LocalDateTime orderDate;
     private OrderStatus orderStatus;
     private Address address;
-  
-  public SimpleOrderDto(Order order) {
-    orderId = order.getId();
-    name = order.getMember().getName();
-    orderDate = order.getOrderDate();
-    orderStatus = order.getStatus();
-    address = order.getDelivery().getAddress();
-    
+
+    public SimpleOrderDto(Order order) {
+      orderId = order.getId();
+      name = order.getMember().getName();
+      orderDate = order.getOrderDate();
+      orderStatus = order.getStatus();
+      address = order.getDelivery().getAddress();
+    }
   }
-  
-}
 }
